9a04935893acdc0ed8742f62a6729e13b81478d0
FinFan <finex.dev@gmail.com>
2024-12-03 22:19:50 +0300

    <коммит, в котором добавлены тесты из пункта 2.2 ('contains')>

--- main.cpp
+++ main.cpp
@@ -27,11 +27,35 @@ bool test_body_contains3() {
     return data.body_contains(1.0);
 }
 
+// проверка содержания цены в границах low/high
+bool test_contains1() {
+    // price >= low && price <= high
+    Candle data(0.0, 1, 0, 0);
+    return data.contains(0.5); // true если 0.5 between 1 & 0
+}
+
+// проверка выхода за нижюю границу low
+bool test_contains2() {
+    // price >= low && price <= high
+    Candle data(0.0, 1, 0, 0);
+    return !data.contains(-1.0); // true если !(price >= low)
+}
+
+// проверка выхода за высшую границу high
+bool test_contains3() {
+    // price >= low && price <= high
+    Candle data(0.0, 1, 0, 0);
+    return !data.contains(2.0); // true если !(price >= low && price !(price <= high))
+}
+
 void initTests()
 {
     tests.push_back(test_body_contains1);
     tests.push_back(test_body_contains2);
     tests.push_back(test_body_contains3);
+    tests.push_back(test_contains1);
+    tests.push_back(test_contains2);
+    tests.push_back(test_contains3);
 }
 
 int launchTests()
