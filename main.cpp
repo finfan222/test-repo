93c26296b39dc1cccf968820434b2893e493bb33
FinFan <finex.dev@gmail.com>
2024-12-03 22:15:36 +0300

    <коммит, в котором добавлены тесты из пункта 2.1 (test_body_contains)>

--- main.cpp
+++ main.cpp
@@ -2,11 +2,36 @@
 #include <functional>
 #include <iostream>
 
+#include "candle.h"
+
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
 
+// для is_green (return price >= open && price <= close;) where close = max (close > open)
+bool test_body_contains1() {
+    Candle data(0.5, 0, 0, 1.0);
+    // 0.99 >= 0.5 && 0.99 <= 1 
+    return data.body_contains(0.99);
+}
+
+// для !is_green (return price >= close && price <= open;) where open = max
+bool test_body_contains2() {
+    Candle data(1, 0, 0, 0.5);
+    // 0.99 >= 1 && 0.99 <= 0.5
+    return data.body_contains(0.99);
+}
+
+// в случае если close = open
+bool test_body_contains3() {
+    Candle data(1.0, 0, 0, 1.0);
+    return data.body_contains(1.0);
+}
+
 void initTests()
 {
+    tests.push_back(test_body_contains1);
+    tests.push_back(test_body_contains2);
+    tests.push_back(test_body_contains3);
 }
 
 int launchTests()
